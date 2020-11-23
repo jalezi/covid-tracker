c286a96d00da8d7e953e1abb2d8267d8dadaaaa8
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2020-11-23 13:54:03 +0100

    utils: remove newDate function

--- src/utils.js/index.js
+++ src/utils.js/index.js
@@ -1,7 +1,4 @@
 import URL_PATH from './urlAndPath';
 export const { URL, PATH } = URL_PATH;
 
-const newDate = date => new Date(date);
-
-export const DATE_FUNCS = Object.freeze({ newDate });
-export default Object.freeze({ DATE_FUNCS, URL, PATH });
+export default Object.freeze({ URL, PATH });
