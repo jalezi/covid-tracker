19fc156177452b3740837423b6e1510ccd88b7da
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2020-11-24 06:31:16 +0100

    App: add World component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
 import './App.css';
 
+import { URL, utilities } from './utils';
+import World from './components/World';
+
+const yesterdayParams = {
+  yesterday: true,
+};
+
+const twoDaysAgoParams = {
+  twoDaysAgo: true,
+};
+
 function App() {
+  const formatStr = 'E, d. MMM yyyy';
+  const today = new Date();
+  const todayRender = utilities.formatDate.date_EN(formatStr)(today);
+
   return (
     <div className="App">
-      <h1>Hello, world!</h1>
+      <header>HEADER</header>
+      <main>
+        <h1>Covid tracker</h1>
+        <p>From NOVEL CoVID19 API: disease.sh/v3/covid</p>
+        <p>Date: {todayRender}</p>
+        <section>
+          <h2>World</h2>
+          <World url={URL.WORLD_ALL_URL} title="today" />
+          <World
+            url={URL.WORLD_ALL_URL}
+            params={yesterdayParams}
+            title="yesterday"
+          />
+          <World
+            url={URL.WORLD_ALL_URL}
+            params={twoDaysAgoParams}
+            title="two days ago"
+          />
+        </section>
+      </main>
+      <footer>FOOTER</footer>
     </div>
   );
 }
