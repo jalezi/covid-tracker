036136fe96754033fb00cc559724bc60f926e696
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2020-11-24 16:10:23 +0100

    App: add footer component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Card from './components/Card';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <main>
         <Card title="world"></Card>
       </main>
-      <footer>FOOTER</footer>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return <footer class="Footer">Design by Jaka Daneu</footer>;
+}
+
+export default Footer;
