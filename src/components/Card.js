d34b9b90ec68efe185cb26a5cb8db8c86d98a1aa
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2020-11-25 06:53:43 +0100

    Card: show buttons

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Card.css';
 import World from './World';
 import { URL } from '../utils';
@@ -12,24 +12,44 @@ const twoDaysAgoParams = {
 };
 
 function Card({ title = 'title' }) {
+  const [expand1, setExpand1] = useState(false);
+  const [expand2, setExpand2] = useState(false);
+
+  const handleExpand1 = () => {
+    setExpand1(prev => !prev);
+    if (expand2) setExpand2(false);
+  };
+  const handleExpand2 = () => {
+    setExpand2(prev => !prev);
+    if (expand1) setExpand1(false);
+  };
+
   return (
     <section className="Card">
       <header>
         <h2 className="card-title">{title}</h2>
       </header>
       <World url={URL.WORLD_ALL_URL} title="today" keyPrefix="today" />
-      <World
-        url={URL.WORLD_ALL_URL}
-        params={yesterdayParams}
-        title="yesterday"
-        keyPrefix="yesterday"
-      />
-      <World
-        url={URL.WORLD_ALL_URL}
-        params={twoDaysAgoParams}
-        title="two days ago"
-        keyPrefix="two-days-ago"
-      />
+      <div className="buttons">
+        <button onClick={handleExpand1}>yesterday</button>
+        <button onClick={handleExpand2}>two days ago</button>
+      </div>
+      {expand1 ? (
+        <World
+          url={URL.WORLD_ALL_URL}
+          params={yesterdayParams}
+          title="yesterday"
+          keyPrefix="yesterday"
+        />
+      ) : null}
+      {expand2 ? (
+        <World
+          url={URL.WORLD_ALL_URL}
+          params={twoDaysAgoParams}
+          title="two days ago"
+          keyPrefix="two-days-ago"
+        />
+      ) : null}
     </section>
   );
 }
