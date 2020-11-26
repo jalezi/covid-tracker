dfbbba7a098fa59b4efd3bc25961e7f5552c6f29
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2020-11-26 18:18:08 +0100

    data, utils: add countries url, path

--- src/data/index.js
+++ src/data/index.js
@@ -1,2 +1,2 @@
-export const PATHS = ['world', 'continents'];
+export const IMPLEMENTED_API_PATHS = ['world', 'continents', 'countries'];
 export { default as makeDataObjectFrom } from './makeDataObjectFrom';
