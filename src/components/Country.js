2d74c41e6b0c3518bfa4e8809ce14e3805157832
Jaka Daneu <44704999+jalezi@users.noreply.github.com>
2020-11-28 12:34:30 +0100

    fix rearnge imports

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './Country.css';
 
-import Select from './Select';
 import useFetch from './../hooks/useFetch';
 import useLocation from './../hooks/useLocation';
 import browserLocation from '../hooks/useLocation/';
